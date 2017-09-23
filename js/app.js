a4adf06d55d44afb31e482036548624d5f0be2f3
Nathan N <Nitharios@users.noreply.github.com>
2017-09-23 09:56:17 -1000

    Started gathering ids for meeting requirements

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
+// jshint esversion:6
+
 console.log('Sanity Check: The Async Force Episode 2');
+
+(function episodeTwo() {
+  const allOptions = document.getElementsByTagName("option");
+  console.log(allOptions);
+  const resourceType = document.getElementById("resourceType");
+
+  const resourceListener = addEventListener("load", resourceSelector());
+
+  // selects the option taken by user
+  resourceSelector = () => {
+    let optionSelected = document.getElementById("resourceType").value;
+    console.log(optionSelected);
+  };
+  
+
+  // () => {};
+
+})();

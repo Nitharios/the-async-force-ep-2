735dde1fdc657f7e2564018bb90e45b72ae42032
Nathan N <Nitharios@users.noreply.github.com>
2017-10-01 16:28:00 -1000

    Able to generate name and species then append to DOM

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ const working = 'It works!';
   const resourceType = document.getElementById("resourceType");
   const resourceId = document.getElementById("resourceId");
   const requestResourceButton = document.getElementById("requestResourceButton");
+  const contentContainer = document.getElementById('contentContainer');
   
   let typeSelected, 
       idSelected;
@@ -40,22 +41,28 @@ const working = 'It works!';
   }
 
   // handles converting data from returned document
-  function dataConverter(info) {
+  function dataConverter(datafile) {
     // this is a link
-    console.log(info);
-    let currentUrl = info[typeSelected];
-    console.log(currentUrl);  
+    console.log(datafile);
+    let currentUrl = datafile[typeSelected];
+    console.log('url', currentUrl);  
 
     // if there is a results key in the object, this will trigger
-    if (info.results) {
-      console.log(working);
+    if (datafile.results) {
+      let results = datafile.results;
+
+      console.log('requested:', results[idSelected].name);
+      contentContainer.appendChild(generateName(results));
+      contentContainer.appendChild(generateDetails(results, 'gender'));
+
       // need to handle the multiple pages issue
 
       // pass in the correct array to the appropriate function
 
     // else it might be a movie title
-    } else if (info[title] || info[name]) {
-      return info[title] || info[name];
+    } else if (datafile.title || datafile.name) {
+      console.log(datafile.title || 'not working!');
+      return datafile.title || datafile.name;
 
     // else it must be a link
     } else {
@@ -63,48 +70,71 @@ const working = 'It works!';
     }
   }
 
-  // data is an array passed in the the dataConverter function
+  // data is passed in as an array
+  // all name tags will appear in an h2 tag
+  function generateName(array) {
+    let name = document.createElement('h2');
+    name.class = 'name';
+    name.innerHTML = array[idSelected].name;
+    return name;
+  }
+
+  // details are placed in a p tag
+  // gender is in the same array as name
+  // species is an array of links --> array.species[0];
+  function generateDetails(array, trait) {
+    let detail = document.createElement('p');
+    
+    if (trait === 'gender') {
+      detail.class = trait;
+      detail.innerHTML = array[idSelected][trait];
+      return detail;
+    }
+  }
+
+
+
+})(window);
+
   // handles the 'person' option
-  function getPerson(arr) {
-    let nameElement = document.createElement('h2');
-    let genderElement = document.createElement('p');
-    let speciesElement = document.createElement('p');
+  // function getPerson(arr) {
+  //   let nameElement = document.createElement('h2');
+  //   let genderElement = document.createElement('p');
+  //   let speciesElement = document.createElement('p');
 
-    let name = arr[idSelected].name;
-    let gender = arr[idSelected].gender;
-    // this is a link
-    let species = retrieveAPI(arr[idSelected].species);
+  //   let name = arr[idSelected].name;
+  //   let gender = arr[idSelected].gender;
+  //   // this is a link
+  //   let species = retrieveAPI(arr[idSelected].species);
 
-  }
+  // }
+  // // handles the 'planet' option
+  // function getPlanet(arr) {
+  //   let nameElement = document.createElement('h2');
+  //   let terrainElement = document.createElement('p');
+  //   let populationElement = document.createElement('p');
+  //   let filmList = document.createElement('ul');
+  //   let filmsElement = document.createElement('li');
 
-  // handles the 'planet' option
-  function getPlanet(arr) {
-    let nameElement = document.createElement('h2');
-    let terrainElement = document.createElement('p');
-    let populationElement = document.createElement('p');
-    let filmList = document.createElement('ul');
-    let filmsElement = document.createElement('li');
+  //   let name = arr[idSelected].name;
+  //   let terrain = arr[idSelected].terrain;
+  //   let population = arr[idSelected].population;
+  //   // this is a link
+  //   let films = retrieveAPI(arr[idSelected].films);
+  // }
 
-    let name = arr[idSelected].name;
-    let terrain = arr[idSelected].terrain;
-    let population = arr[idSelected].population;
-    // this is a link
-    let films = retrieveAPI(arr[idSelected].films);
-  }
+  // // handles the 'starship' option
+  // function getStarship(arr) {
+  //   let nameElement = document.createElement('h2');
+  //   let manufacturerElement = document.createElement('p');
+  //   let starshipClassElement = document.createElement('p');
+  //   let filmsList = document.createElement('ul');
+  //   let filmsElement = document.createElement('li');
 
-  // handles the 'starship' option
-  function getStarship(arr) {
-    let nameElement = document.createElement('h2');
-    let manufacturerElement = document.createElement('p');
-    let starshipClassElement = document.createElement('p');
-    let filmsList = document.createElement('ul');
-    let filmsElement = document.createElement('li');
-
-    let name = arr[idSelected].name;
-    let manufacturer = arr[idSelected].manufacturer;
-    let starshipClass = arr[idSelected].starship_class;
-    // this is a link
-    let films = retrieveAPI(arr[idSelected].films);
-  }
-
-})();
+  //   let name = arr[idSelected].name;
+  //   let manufacturer = arr[idSelected].manufacturer;
+  //   let starshipClass = arr[idSelected].starship_class;
+  //   // this is a link
+  //   let films = retrieveAPI(arr[idSelected].films);
+  //   console.log(films);
+  // }
